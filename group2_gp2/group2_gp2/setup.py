ac73b6aaec4762e47e84a3335729ffd327cc3497
namfacchetti <126823581+namfacchetti@users.noreply.github.com>
2026-04-25 20:42:57 -0400

    Added Comments
    Added some comments to the parts I worked on, need to check one last thing and test

---
+++ group2_gp2/group2_gp2/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'group2_gp2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'),
+            glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='maxykumd , namfacchetti',
+    maintainer_email='maxyk@umd.edu, gfacchet@terpmail.umd.edu',
+    description='ROS 2 action server and client for GP2 navigate-to-goal mission using a two-phase proportional controller.',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'navigate_to_goal_server = group2_gp2.scripts.main_navigate_to_goal_server:main',
+        	'navigate_to_goal_client = group2_gp2.scripts.main_navigate_to_goal_client:main',
+        ],
+    },
+)
